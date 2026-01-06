8e7542b88b65c42b7d0c48e96721eea0d7f27685
Gabriel <gabriel@Gabriels-MacBook-Air.local>
2026-01-06 14:45:31 +0100

    oky

--- components/sinclair_ac/esppac_cnt.cpp
+++ components/sinclair_ac/esppac_cnt.cpp
@@ -299,7 +299,7 @@ void SinclairACCNT::send_packet()
             fanSpeed2 = 1;
             fanQuiet  = true;
             fanTurbo  = false;
-            packet[protocol::REPORT_FAN_SPD2_BYTE] |= 5;
+            packet[protocol::REPORT_FAN_SPD2_BYTE] |= 1;
         } 
         else if (strcmp(custom_fan_mode, fan_modes::FAN_MED) == 0)
 	{
